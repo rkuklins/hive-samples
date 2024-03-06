f8399b66184e2fb9a7ec3f76d6c20e6043a27267
Rafal <rav.kuklinski@gmail.com>
2024-03-06 10:42:56 +0100

    Code to run Hive on set of texts we used also with AF vendor for content moderation

--- hive-text-moderation.py
+++ hive-text-moderation.py
@@ -2,45 +2,72 @@
 # Text API requests
 
 import requests
+import csv
+
+# Specify the path of the CSV file
+csv_input_file_name = '/Users/rafalkuklinski/dev/SC/hive-samples/sample_texts/random_texts.csv'
+csv_output_file_name = '/Users/rafalkuklinski/dev/SC/hive-samples/sample_texts/hive_response_random.csv'
+
+is_first_line = True
+
+
 
 headers = {
-    'Authorization': 'Token YOUR_API_KEY',
+    'Authorization': 'Token p5CtNSI2XSmdzfs1E1o2fypuaW0yQ1gE',
 }
 
 data = {
-  'text_data': 'My input string goes here.',
+  'text_data': 'Suck My Pussy Nigga Every Opp Shot Nigga Gang Gang Nigga',
   'metadata': '{"my_UUID": "3c78fc82-f797-11ea-adc1-0242ac120002"}'
 }
 
-response = requests.post('https://api.thehive.ai/api/v2/task/sync',
-                          headers=headers, 
-                          data=data)
+texts = []
+text_no=0;
 
-response_string = response.text 
-response_dict = response.json()
+# Load CSV file into a structure
+with open(csv_input_file_name, 'r') as file:
+  reader = csv.reader(file)
+  for row in reader:
+    texts.append(row[0])  # Assuming the text is in the first column of the CSV
 
-# -------------------------- #
-# submit a task with specified language (speech/text moderation, transcription only)
-# see docs for supported language
-import requests
+# Open the CSV file in write mode
+with open(csv_output_file_name, 'w', newline='') as csv_file:
+  # Create a CSV writer object
+  writer = csv.writer(csv_file)
+  # Iterate over all texts in the sample file
+  for text in texts:
+    print("Text [" + str(text_no) + "]:" + text)
+    text_no = text_no + 1;
+    
+    #Each text from a file add to the "data" structure for hive request
+    data['text_data'] = text
 
-headers = {
-    'Authorization': 'Token YOUR_API_KEY',
-}
+    # Make the API request to hive
+    response = requests.post('https://api.thehive.ai/api/v2/task/sync',
+                headers=headers,
+                data=data)
 
-files = {'image': open('my_path/my_image.jpg', 'rb')}
+    # Process the response
+    response_string = response.text
+    response_dict = response.json()
 
-data = {
-  'metadata': '{"my_UUID": "3c78fc82-f797-11ea-adc1-0242ac120002"}',
-  'options': '{"input_language":"es"}'
-}
+    try:
+        #If this is first line, get all the headers and add to the CSV file
+        if(is_first_line):
+            
+            line = ["file name", "text"];
+            for reco_class in response_dict['status'][0]['response']['output'][0]['classes']:
+                line.append(str(reco_class['class']))
+                
+            writer.writerow(line)
+            is_first_line = False
 
-response = requests.post('https://api.thehive.ai/api/v2/task/sync',
-                          headers=headers, 
-                         	files=files,
-                          data=data)
+        # In any case process all the values and add a line
+        line = [csv_input_file_name, text];
+        for reco_class in response_dict['status'][0]['response']['output'][0]['classes']:
+            line.append(str(reco_class['score']))
 
-response_string = response.text 
-response_dict = response.json()
-
+        writer.writerow(line)   
+    except:
+        print("Error processing: " + file_name)
 # -------------------------- #
